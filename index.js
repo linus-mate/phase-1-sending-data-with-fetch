37229deca384d7c1945804d9c9ae27f8e579e91a
linus mutugi mate <mlynux@gmail.com>
2022-08-28 18:17:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+function submitData(name, email){
+    const userData = {
+        name: name,
+        email: email
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(userData),
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function(user){
+          const p = document.createElement('p');
+          p.textContent = user.id;
+          document.querySelector('body').appendChild(p);
+        })
+
+        .catch(function(error){
+          const p = document.createElement('p');
+
+          p.textContent = error.message;
+
+          document.querySelector('body').appendChild(p);
+        })
+        
+};
+ 
+  
+
+
+
+
+  
